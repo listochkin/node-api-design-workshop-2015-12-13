fda8597636455804d340bfa7530a3c05877795c0
Andrey Listochkin <listochkin@outlook.com>
2015-12-13 11:41:36 +0200

    switched to generators for async code

--- test/getting-started.js
+++ test/getting-started.js
@@ -3,7 +3,8 @@
 const chai = require('chai'),
   assert = chai.assert;
 
-const fetch = require('node-fetch');
+const fetch = require('node-fetch'),
+  co = require('co');
 
 const server = require('../src/hello-server');
 
@@ -11,10 +12,10 @@ describe('hello world', () => {
   before(done => server.listen(4000, done));
   after(() => server.close());
 
-  it('shold respond to requests', done => {
-    fetch('http://localhost:4000/').then(response => {
-      assert(response.ok, 'hello world response');
-      done();
-    });
-  });
+  it('shold respond to requests', co.wrap(function* () {
+    const response = yield fetch('http://localhost:4000/');
+    assert(response.ok, 'hello world response');
+    const text = yield response.text();
+    assert(text === 'Hello World\n', 'showl say hello world');
+  }));
 });
